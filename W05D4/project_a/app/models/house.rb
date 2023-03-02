f86e4af2e34c0a391c90f3d9709710d5b33a8aa8
David M Gudeman <davidmgudeman@gmail.com>
2023-03-02 07:58:43 -0800

    completed the homeowork

--- W05D4/project_a/app/models/house.rb
+++ W05D4/project_a/app/models/house.rb
@@ -4,5 +4,13 @@ require_relative 'application_record'
 
 class House <ApplicationRecord
 
+    has_many(
+        :residents,
+        class_name: 'Person',
+        foreign_key: :house_id,
+        primary_key: :id
+      )
+    validates :address, presence: true
+
 
 end
--- W05D4/project_a/app/models/person.rb
+++ W05D4/project_a/app/models/person.rb
@@ -1,6 +1,13 @@
 require_relative 'application_record'
 
 class Person <ApplicationRecord
+
+    belongs_to(
+        :house,
+        class_name: 'House',
+        foreign_key: :house_id,
+        primary_key: :id
+      )
     validates :name, presence: true
 
 
